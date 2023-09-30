76e0f6e218f7269a7f66c6c20385cea7dbf95447
watanabe-tsubasa <t.watanabe423@gmail.com>
2023-09-30 16:42:39 +0900

    upd

--- server/src/modules/handler.ts
+++ server/src/modules/handler.ts
@@ -1,4 +1,4 @@
-import { Client, ClientConfig, MessageAPIResponseBase, WebhookEvent } from "@line/bot-sdk";
+import { MessageAPIResponseBase, WebhookEvent } from "@line/bot-sdk";
 import { createFish, growUpFish, rebirthFish, struggleToInvader } from "./lineFuncs";
 
 export const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIResponseBase | undefined> => {
@@ -11,14 +11,14 @@ export const textEventHandler = async (event: WebhookEvent): Promise<MessageAPIR
 
   switch (text) {
     case '生み出す':
+      // userIdがkintone上に存在しない場合
       createFish(userId, replyToken, text);
+      // userIdがkintone上に存在する場合
+      rebirthFish(userId, replyToken, text);
       break;
     case '釣り人':
     case 'まぐろ漁船':
       struggleToInvader(userId, replyToken, text);
-      break;
-    case '転生する':
-      rebirthFish(userId, replyToken, text);
       break;
     default:
       growUpFish(userId, replyToken, text);
--- server/src/modules/lineFuncs.ts
+++ server/src/modules/lineFuncs.ts
@@ -1,4 +1,4 @@
-import { Client, ClientConfig, MessageAPIResponseBase, TextMessage, WebhookEvent } from "@line/bot-sdk";
+import { Client, ClientConfig, TextMessage } from "@line/bot-sdk";
 import { gptReply } from "./gptReply";
 
 const clientConfig: ClientConfig = {
@@ -9,6 +9,7 @@ const client = new Client(clientConfig);
 
 export const createFish = async (userId:string, replyToken: string, text: string) => {
   console.log('createFish');
+  // kintone上にデータを作成し、全初期ステータスが0のデータを作り、ししゃもの絵を描く
 }
 
 export const growUpFish = async (userId:string, replyToken: string, text: string) => {
@@ -24,20 +25,34 @@ export const growUpFish = async (userId:string, replyToken: string, text: string
 }
 
 export const struggleToInvader = async (userId:string, replyToken: string, text: string) => {
+  // （isTrained = false, isBattled = false, rebirthCount++）
+  // database: （isTrained = trueのとき isBattled = true）
   console.log('struggleToInvader');
   switch (text) {
     case '釣り人':
       console.log("釣り人と戦います");
+      // 5つのパラメーター合計が35以下の場合ししゃもフライとなって、データが初期化される
       break;
-    case 'まぐろ漁船':
-      console.log("まぐろ漁船と戦います");
-      break;
-    default:
-      console.log("不明なエラーです");
-      break;
-  }
-}
+      case 'まぐろ漁船':
+        console.log("まぐろ漁船と戦います");
+        // 5つのパラメーター合計が45以下の場合ししゃもフライとなって、データが初期化される
+        break;
+        default:
+          console.log("不明なエラーです");
+          break;
+        }
+      }
 
 export const rebirthFish = async (userId:string, replyToken: string, text: string) => {
+  // database:（isTrained = true&&isBattled = true のとき isTrained = false, isBattled = false, rebirthCount++）にし、新たにししゃもの絵を描く
   console.log('rebirthFish');
+
+  const replyMessage = '生き残ったししゃもはたくさんの卵を残し、次の世代に力を引き継ぎました';
+
+  const response: TextMessage = {
+    type: 'text',
+    text: replyMessage
+  };
+
+  await client.replyMessage(replyToken, response);
 }
