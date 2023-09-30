38a4cae9aa148c23a9148ec0ed061820239befe1
watanabe-tsubasa <t.watanabe423@gmail.com>
2023-09-30 14:08:06 +0900

    upd

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,36 @@
 import { Hono } from "hono";
-import { serveStatic } from "hono/serve-static.bun";
+import { serveStatic } from "hono/bun";
+import { WebhookEvent } from "@line/bot-sdk";
+import { textEventHandler } from "./modules/handler";
 
-const port = parseInt(process.env.PORT) || 3000;
+
+const portString = process.env.PORT;
+const port = portString ? parseInt(portString) : 3000;
 
 const app = new Hono();
 
 app.use("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }));
-
 app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
+  return c.json({ message: "Hello LINE Bot!" });
+});
+app.post("/webhook", async (c) => {
+  const data = await c.req.json();
+  const events: WebhookEvent[] = (data as any).events;
+  await Promise.all(
+    events.map(async (event: WebhookEvent) => {
+      try {
+        await textEventHandler(event);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          console.error(err);
+        }
+        return c.json({
+          status: "error",
+        });
+      }
+    })
+  );
+  return c.json({ message: "ok" });
 });
 
 console.log(`Running at http://localhost:${port}`);
---
+++ server/src/modules/systemPrompt.ts
@@ -0,0 +1,3 @@
+export const systemPrompt = `
+日本語で返答してください
+`;
